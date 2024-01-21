901f9dbf874344772dc01f42bdca36dbca8966ac
Josh Magness <magnessjo@gmail.com>
2024-01-21 16:51:49 -0500

    Design header concept

--- source/scripts/app.ts
+++ source/scripts/app.ts
@@ -1,1 +1,20 @@
+const MenuBehaviors = () => {
+  const menu = document.querySelector('menu');
+  const main = document.querySelector('main');
+
+  if (!menu || !main) return;
+
+  const menuDisplay = () => {
+    const menuPosition = menu.getBoundingClientRect().top;
+    const mainPositon = main.getBoundingClientRect().top;
+  };
+
+  menuDisplay();
+  window.addEventListener('scroll', menuDisplay);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  MenuBehaviors();
+});
+
 export {};
